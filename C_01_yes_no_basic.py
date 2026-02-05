3c018d25a05fcd187dc6a1e3cfecb0e434a12329
Pickles <taylord4062@masseyhigh.school.nz>
2026-02-05 13:21:04 +1300

    Add files via upload

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -1,11 +1,16 @@
 # Ask if the User wants to play
 #respond appropriately
-want_instructions =input("do you want to see the instructions?").lower()
 
-if want_instructions == "yes" or want_instructions == "y":
-    print("You said yes")
-elif want_instructions == "no" or want_instructions == "n":
-    print("You said no")
-else:
-    print("Please enter yes/no")
 
+while True:
+    want_instructions = input("do you want to see the instructions?").lower()
+    if want_instructions == "yes" or want_instructions == "y":
+        print("You said yes")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("You said no")
+        break
+    else:
+        print("Please enter yes/no")
+
+print("we done")
