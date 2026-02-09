fe4a561fe89862641d84200baf63a9cb9dc6b0a3
Pickles <taylord4062@masseyhigh.school.nz>
2026-02-09 14:49:17 +1300

    Add files via upload

--- C_01_yes_no_basic_v2.py
+++ C_01_yes_no_basic_v2.py
@@ -3,17 +3,22 @@
 
 def yes_no(question):
 
+    """Checks user response to a question is yes / no (y/n), returns 'yes' or 'no' """
+
     while True:
+
         response = input(question).lower()
+
         if response == "yes" or response == "y":
-            return "yes"
+                return "yes"
         elif response == "no" or response == "n":
-            return "no"
+                return "no"
         else:
-            print("Please enter yes/no")
+                print("Please enter yes/no")
 
-#main routine
+#testing loop
+while True:
+    want_instructions = yes_no("do you want to see the instructions? ")
+    print(f"you chose {want_instructions}")
 
-want_instructions = yes_no("do you want to see the instructions?").lower()
-want_coffee = yes_no()
 print("we done")
